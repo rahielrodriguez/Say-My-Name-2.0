1e452bbe3a0a42687fb36ecd71798469fd6678e6
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-01-25 08:31:58 -0700

    Header added, Do Loop and Leave Variable deleted because they were not useful.

--- SayMyNameBetter.vb
+++ SayMyNameBetter.vb
@@ -1,33 +1,33 @@
-﻿Imports System.Timers
+﻿'Rahiel Rodriguez
+'RCET 2265
+'Say My Name Better
+'Spring 2024
+'https://github.com/rahielrodriguez/SayMyName2.0.git
+
+Option Explicit On
+Option Strict On
 
 Module SayMyNameBetter
 
     Sub Main()
 
         Dim userInput As String
-        Dim leave As Boolean = False
 
         Console.WriteLine("What is your name?")
         userInput = Console.ReadLine()
 
-        Do
-            If userInput = "Emily" Then
-                Console.WriteLine("Hello, Emily! Good to see you.")
-                leave = True
-            ElseIf userInput = "Joe" Then
-                Console.WriteLine("Hello, Joe! Good to see you.")
-                leave = True
-            ElseIf userInput = "Rahiel" Then
-                Console.WriteLine("Hello, Rahiel. I'm glad to see you again" & vbNewLine _
+        If userInput = "Emily" Then
+            Console.WriteLine("Hello, Emily! Good to see you.")
+        ElseIf userInput = "Joe" Then
+            Console.WriteLine("Hello, Joe! Good to see you.")
+        ElseIf userInput = "Rahiel" Then
+            Console.WriteLine("Hello, Rahiel. I'm glad to see you again" & vbNewLine _
                                   & "Hola, Rahiel. Encantado de verte de nuevo")
-                leave = True
-            Else
-                Console.WriteLine("Welcome to this program.")
-                leave = True
-            End If
-        Loop Until leave
+        Else
+            Console.WriteLine("Welcome to this program.")
+        End If
 
-        Console.Read()
+            Console.Read()
     End Sub
 
 End Module
